8a36dcc1fdae9443a4861e7146c8c44be4a02d76
Lukas Schmid <schmluk@mavt.ethz.ch>
2023-01-03 14:27:00 +0100

    remove old commented code

--- lidar_motion_detection_ros/include/lidar_motion_detection_ros/visualization/motion_visualizer.h
+++ lidar_motion_detection_ros/include/lidar_motion_detection_ros/visualization/motion_visualizer.h
@@ -62,57 +62,6 @@ class MotionVisualizer {
                                   const CloudInfo& cloud_info,
                                   const Clusters& clusters);
 
-  // Old.
-  // struct visTypePointcloud {
-  //   ros::Publisher publisher;
-  //   pcl::PointCloud<pcl::PointXYZRGB> cloud;
-  // };
-
-  // typedef struct visTypeMarker {
-  //   ros::Publisher publisher;
-  //   visualization_msgs::MarkerArray marker_arr;
-  // };
-  // void initializePointcloudPublishers();
-  // void setupColors();
-  // void setAllCloudsToVisualize(
-  //     const pcl::PointCloud<pcl::PointXYZ>& processed_pcl);
-  // void publishAll();
-  // void setEverFreeLevelDetectionsCloud(
-  //     const pcl::PointCloud<pcl::PointXYZ>& processed_pcl);
-  // void setClusterLevelDetectionsCloud();
-  // void setObjectLevelDetectionsCloud();
-  // void setGroundTruthDetectionsCloud(
-  //     const pcl::PointCloud<pcl::PointXYZ>& processed_pcl);
-  // void setLidarPointcloudWithoutDynamicPoints(
-  //     const pcl::PointCloud<pcl::PointXYZ>& processed_pcl);
-  // void setNeverFreeVoxelsCloud();
-  // void setEverFreeSliceCloud();
-  // void initNewPointcloudToVisualize(const std::string& topic_name,
-  //                                   const std::string& frame);
-  // void initNewPointcloudToVisualize(const std::string& topic_name);
-  // void initNewMarkerArrayToVisualize(const std::string& topic_name);
-  // ros::Publisher* getPointcloudPublisher(const std::string& topic_name);
-  // ros::Publisher* getMarkerPublisher(const std::string& topic_name);
-  // pcl::PointCloud<pcl::PointXYZRGB>* getPointcloud(
-  //     const std::string& topic_name);
-  // void setPointcloud(const std::string& topic_name,
-  //                    const pcl::PointCloud<pcl::PointXYZ>& pointcloud,
-  //                    const voxblox::Color& color);
-  // void clearPointcloud(const std::string& topic_name);
-  // void clearMarkerArray(const std::string& topic_name);
-  // void publishPointcloud(const std::string& topic_name);
-  // void publishMarkerArray(const std::string& topic_name);
-  // void addMarkerToArray(const std::string& topic_name,
-  //                       visualization_msgs::Marker* marker);
-  // void addPointToPointcloud(const std::string& topic_name,
-  //                           const pcl::PointXYZ& point,
-  //                           const voxblox::Color& color);
-  // void initMaxRaylengthVisualizer(const float& max_raylength,
-  //                                 const voxblox::Color& color,
-  //                                 const std::string& frame,
-  //                                 const int num_points = 100);
-  // void publishMaxRaylengthCircle();
-
  private:
   const Config config_;
   voxblox::ExponentialOffsetIdColorMap color_map_;
@@ -130,25 +79,6 @@ class MotionVisualizer {
   ros::Publisher detection_object_comp_pub_;
   ros::Publisher ever_free_pub_;
   ros::Publisher never_free_pub_;
-
-  // Old.
-  // CloudInfo* point_classifications_ptr_;
-  // std::vector<Cluster>* current_clusters_ptr_;
-
-  // std::string world_frame_;
-  // std::string sensor_frame_;
-
-  // MaxRaylengthIndicator max_raylength_circle_;
-  // ros::Publisher max_raylength_pub_;
-
-  // std::unordered_map<std::string, visTypePointcloud>
-  // pointcloud_vis_container_; std::unordered_map<std::string, visTypeMarker>
-  // marker_vis_container_;
-
-  // voxblox::Color point_and_cluster_candidate_color_;
-  // voxblox::Color never_free_color_;
-  // voxblox::Color lidar_point_color_;
-  // voxblox::Color sensor_origin_color_;
 };
 
 }  // namespace motion_detection
